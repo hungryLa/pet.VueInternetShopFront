03689222ea23b73b99cc0e6c7886f193efe3ccba
niyaz <hungry_la@mail.ru>
2023-07-19 16:51:12 +0500

    Урок №13 -Научился получать данные из env

--- src/stores/counter.js
+++ src/stores/counter.js
@@ -3,6 +3,7 @@ import { defineStore } from 'pinia'
 
 export const useCounterStore = defineStore('counter', () => {
   const count = ref(0)
+  const APP_URL = 'http://127.0.0.1:8000/'
   const doubleCount = computed(() => count.value * 2)
   function increment() {
     count.value++
