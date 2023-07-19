800daa1845b048faf1c6e0763c6173bc913b1306
niyaz <hungry_la@mail.ru>
2023-07-19 15:24:34 +0500

    Урок №11 Накидывал страницы и настраивал шапку

---
+++ src/views/product/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    Продукты
+  </div>
+</template>
+
+<script>
+export default {
+  name: "index.vue",
+  mounted() {
+    $(document).trigger('change')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
